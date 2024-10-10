f53b1849d5f14bb81ee60db535ac90fcc7d37702
Praktikant471 <m.steinb@web.de>
2024-10-10 10:41:08 +0200

    Postgres Init

--- src/main/java/com/example/noidontwantthat_java/HelloApplication.java
+++ src/main/java/com/example/noidontwantthat_java/HelloApplication.java
@@ -1,20 +1,47 @@
 package com.example.noidontwantthat_java;
 
+import com.example.noidontwantthat_java.controller.HelloController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    HelloController controller = new HelloController();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // Button für Neue Tätigkeiten
+        Button newtask = new Button("Neue Tätigkeit anlegen");
+        newtask.setOnAction(e -> {
+            // Öffnen
+            controller.handleNewTask(null);
+        });
+
+        // Button für Tätigkeit einsehen
+        Button viewtask = new Button("Tätigkeiten einsehen");
+        viewtask.setOnAction(e -> {
+            // Einsehen
+            controller.handleViewTask(null);
+        });
+
+        VBox layout = new VBox(10); // 10px Abstand zw Buttons
+        layout.getChildren().addAll(newtask, viewtask);
+
+        // Szene und Fenster
+        Scene scene = new Scene(layout, 320, 240);
+        stage.setTitle("Tätigkeits-Manager!");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/noidontwantthat_java/controller/HelloController.java
+++ src/main/java/com/example/noidontwantthat_java/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.noidontwantthat_java.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +17,40 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void handleNewTask(ActionEvent event) {
+        try {
+            // FXML laden
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com.example.noidontwantthat_java/new-task-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // neue Szene (Fenster)
+            Stage stage = new Stage();
+            stage.setTitle("Neue Tätigkeit");
+            stage.setScene(new Scene(root, 400, 300));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void handleViewTask(ActionEvent event) {
+        try {
+            // FXML laden
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com.example.noidontwantthat_java/view-task.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // neue Szene
+            Stage stage = new Stage();
+            stage.setTitle("Tätigkeiten einsehen");
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
